a403621b724540aef54669b202ef417a7ae3fe8f
annlesss <leshchineranna@gmail.com>
2022-06-16 16:39:20 +0300

    дз 16.06

--- Атрибуты и методы/Домашнее задание/task4_Date_classmethod_staticmethod/main.py
+++ Атрибуты и методы/Домашнее задание/task4_Date_classmethod_staticmethod/main.py
@@ -16,17 +16,29 @@ class Date:
 
     def is_leap_year(self, year: int):
         """Проверяет, является ли год високосным"""
-        ...  # TODO
+
+        if self.year % 4 == 0:
+            return True
+        else:
+            return False
 
     def get_max_day(self, month: int, year: int):
         """Возвращает максимальное количество дней в месяце для указанного года"""
-        ...  # TODO
+        return DAY_OF_MONTH[0][0] # TODO
 
     def is_valid_date(self, day: int, month: int, year: int):
         """Проверяет, является ли дата корректной"""
-        ...  # TODO
+        if day > self.get_max_day(month, year):  # TODO
+            raise TypeError
+        if month > 12:
+            raise TypeError
+        print("дата корректна")
 
 
 if __name__ == "__main__":
     # Write your solution here
-    pass
+    DAY_OF_MONTH = (
+        (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31),  # обычный год
+        (31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)  # високосный
+    )
+    date = Date(16, 9, 2003)

8a8ca5e9dce3f8e735557420b1cdc01d732c5536
annlesss <leshchineranna@gmail.com>
2022-06-23 21:19:11 +0300

    практика 23.06

--- Паттерны/Лабораторные задания/task2_strategy/main.py
+++ Паттерны/Лабораторные задания/task2_strategy/main.py
@@ -11,6 +11,13 @@ class LinkedListWithDriver(LinkedList):
         self.driver = driver
 
     # TODO свойство для driver (getter + setter)
+    @property
+    def driver(self):
+        return self._driver
+    @driver.setter
+    def driver(self, driver: IStructureDriver):
+        self._driver = driver
+
 
     def read(self):
         """ С помощью драйвера считать данные и поместить их в LinkedList. """
@@ -26,9 +33,9 @@ class LinkedListWithDriver(LinkedList):
 
 if __name__ == '__main__':
     ll = LinkedListWithDriver()
-    ll.driver = ...  # TODO инициализировать SimpleFileDriver
+    ll.driver = SimpleFileFactoryMethod.get_driver()  # TODO инициализировать SimpleFileDriver
     ll.read()
     print(ll)
 
-    ll.driver = ...  # TODO инициализировать JsonFileDriver
+    ll.driver = JsonFileDriverFactoryMethod.get_driver()  # TODO инициализировать JsonFileDriver
     ll.write()
--- Паттерны/Лабораторные задания/task3_Driver_faсtory_method_json/main.py
+++ Паттерны/Лабораторные задания/task3_Driver_faсtory_method_json/main.py
@@ -24,7 +24,17 @@ class SimpleFileFactoryMethod(DriverFactoryMethod):
 
 
 # TODO реализовать класс JsonFileDriveFactoryMethod
+class JsonFileDriverFactoryMethod(DriverFactoryMethod):
+    DEFAULT_NAME = 'untitled.json'
 
+    @classmethod
+    def get_driver(cls) -> IStructureDriver:
+        filename = input('Введите название json файла: (.json)').strip()
+        filename = filename or cls.DEFAULT_NAME
+        if not filename.endswith('.json'):
+            filename = f'{filename}.json'
+
+        return JsonFileDriver(filename)
 
 if __name__ == '__main__':
     driver = JsonFileDriverFactoryMethod.get_driver()
